8990ea80a91f16d536b28c910202e6cf006cd9b3
Ivan Kulagin <ivan.kulagin2001@gmail.com>
2022-05-14 01:30:09 +0300

    changed app.js and production config

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const PORT = config.get('port') || 5000
+const PORT = config.get('port') || process.env.PORT
 
 async function start() {
   try {
